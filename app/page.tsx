5684b5d5de2075bbeed4e92c93dd8adf5d3f5af8
RandomPerson5571 <ethanguan5571@gmail.com>
2026-05-24 17:30:55 -0400

    added vercel analytics and speed insights

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import DashboardPage from "./pages/DashboardPage";
 
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 export default function Home() {
-  return <DashboardPage />;
+  return (
+    <>
+      <DashboardPage />;
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
 }
